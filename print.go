da95e9b0cab8d7aa0ee0bb7b85809159d20c4f8e
Eltrac <hi@guhub.cn>
2025-11-11 16:21:15 +0800

    style: tidy up print.go

--- print.go
+++ print.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 )
 
-// PrintInfo prints info for either a formula or a cask.
 func PrintInfo(formula *FormulaInfo, cask *CaskInfo, pkgName string) {
 	if formula == nil && cask == nil {
 		fmt.Println("Package not found.")
@@ -14,16 +13,14 @@ func PrintInfo(formula *FormulaInfo, cask *CaskInfo, pkgName string) {
 	}
 
 	printHeader(formula, cask)
-	printDescription(formula, cask)
+	printInstallDesc(formula, cask)
 	printVersionInfo(formula, cask)
 	printMetadata(formula, cask)
 
 	fmt.Println()
 }
 
-// ------------------- Helper functions -------------------
-
-// printHeader prints the icon and package name
+// prints the icon and package name
 func printHeader(formula *FormulaInfo, cask *CaskInfo) {
 	var icon, typ, name, desc string
 
@@ -47,29 +44,31 @@ func printHeader(formula *FormulaInfo, cask *CaskInfo) {
 	fmt.Printf(" - %s\n\n", desc)
 }
 
-// printDescription prints installation description for formulae
-func printDescription(formula *FormulaInfo, cask *CaskInfo) {
-	if formula != nil {
-		if len(formula.Installed) == 0 {
-			return
-		} else if formula.Installed[0].InstalledOnRequest {
-			color.Blue("You installed this package by running `brew install`.\n")
-		} else if formula.Installed[0].InstalledAsDependency {
-			color.Blue("This package was installed automatically as a dependency.\n")
-			// list reverse dependencies
-			dependencies, err := GetReverseDependencies(formula.Name)
-			if err != nil {
-				fmt.Printf("Error getting reverse dependencies: %v\n", err)
-			} else {
-				fmt.Printf("Used by: %s\n", strings.Join(dependencies, ", "))
-			}
-		}
+// answers questions like "how it got here?"
+func printInstallDesc(formula *FormulaInfo, cask *CaskInfo) {
+	if cask != nil || formula == nil {
+		return	// formulae only
 	}
 
+	if len(formula.Installed) == 0 {
+		return
+	} else if formula.Installed[0].InstalledOnRequest {
+		color.Blue("You installed this package by running `brew install`.\n")
+	} else if formula.Installed[0].InstalledAsDependency {
+		color.Blue("This package was installed automatically as a dependency.\n")
+		// list reverse dependencies
+		dependencies, err := GetReverseDependencies(formula.Name)
+		if err != nil {
+			fmt.Printf("Error getting reverse dependencies: %v\n", err)
+		} else {
+			fmt.Printf("Used by: %s\n", strings.Join(dependencies, ", "))
+		}
+	}
 	fmt.Println()
 }
 
-// printVersionInfo prints the latest version and status
+// prints badges like [Outdated] [Up to date] ...
+// used by printMetadata
 func printVersionInfo(formula *FormulaInfo, cask *CaskInfo) {
 	var installedVersion string
 	var installBadge string
@@ -109,7 +108,7 @@ func printVersionInfo(formula *FormulaInfo, cask *CaskInfo) {
 	color.New(installBadgeColor).Printf(" %s\n", installBadge)
 }
 
-// printMetadata prints homepage, license, and other fields
+// prints homepage, license, and other fields
 func printMetadata(formula *FormulaInfo, cask *CaskInfo) {
 	if formula != nil {
 		fmt.Printf("🔗 %s\n", formula.Homepage)
